41b278a498c8ca1cbfa0509236d4e47ddbd96d65
Zorannn123 <tadicz84@gmail.com>
2023-05-11 20:27:43 +0200

    Unos i validacija datuma za klijenta

--- Projekat/Client/Klijent.cs
+++ Projekat/Client/Klijent.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,46 @@ namespace Client
         public void RadKlijenta(IServer kanal)
         {
             // Unos i validacija datuma
+            string format = "dd-mm-yyyy"; //naveden random format, ispravicu kad budem video kako je u bazi napisano
+            DateTime datum;
 
+            while (true)
+            {
+                Console.WriteLine("Unos datuma(dd-mm-yyyy): ");
+                string input = Console.ReadLine();
+                if (DateTime.TryParseExact(input, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out datum))
+                {
+                    Console.WriteLine("DATE: " + datum);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Pogresan unos datuma!");
+                }
+            }
             // Poziv serverske metode
             // List<Load> = kanal.UpitOdKlijenta(datum);
+            kanal.UpitOdKlijenta(datum);
+            Console.WriteLine("prosledjen datum {0}", datum);
 
             // Čuvanje rezultata pretrage u CSV fajl
             // Ispis poruke o kreiranoj datoteci (poruka sadrži i podatke o putanji i imenu datoteke)
+        }
+       
+
+        //ovo vrv nije dobro
+        public Tuple<List<Load>, Audit> UpitOdKlijenta(DateTime datum)
+        {
+            Load load = new Load();
+            load.Timestamp = datum;
+
+            List<Load> list = new List<Load>();
+            list.Add(load);
+            
+            Tuple<List<Load>, Audit> rez = new Tuple<List<Load>, Audit>(list, null);
+            
+
+            return rez;
         }
 
         // Nikola
